9296f2a602295508c6a66b7e04b1962857fa813a
EinWesen <einwesen@gmail.com>
2019-01-22 20:35:57 +0100

    change command name

--- src/extension.ts
+++ src/extension.ts
@@ -40,7 +40,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('einwesen.split-snippet-transform.commands.split_with_clipboard', () => {
 		
 		// The code you place here will be executed every time your command is executed
 		vscode.env.clipboard.readText().then(function (snippetText) {
